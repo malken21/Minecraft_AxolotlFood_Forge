2c1e95d47c230594bf37e87a09dd144e6be8ec3e
malken21 <Malken_Marumasa@outlook.jp>
2022-10-09 13:39:22 +0900

    Delete src/main directory

--- src/main/java/marumasa/axolotl_food/item/ModFoods.java
+++
@@ -1,7 +0,0 @@
-package marumasa.axolotl_food.item;
-
-import net.minecraft.world.food.FoodProperties;
-
-public class ModFoods {
-    public static final FoodProperties AXOLOTL_FOOD = (new FoodProperties.Builder()).fast().nutrition(2).saturationMod(0.2F).build();
-}
